b0e3c24adfeeb85d9886a139abcbbdd2f55c1148
progyu <rocket7101@gmail.com>
2020-01-10 17:15:52 +0900

    Change proxy

---
+++ todo/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/': {
+                target: 'http://localhost:3000/',
+                changeOrigin: true
+            }
+        }
+    },
+    publicPath: "./",
+    outputDir: "../server/public"
+}
